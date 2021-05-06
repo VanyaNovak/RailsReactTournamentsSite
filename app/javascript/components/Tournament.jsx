3af9a1d9da09112f299293d3b31275dfd23649e1
Gelvan <vananovak@ukr.net>
2021-05-06 04:20:57 +0300

    add view tournaments feature

---
+++ app/javascript/components/Tournament.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Tournament extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { tournament: { team: "" } };
+
+    this.addHtmlEntities = this.addHtmlEntities.bind(this);
+  }
+
+  componentDidMount() {
+    const {
+      match: {
+        params: { id }
+      }
+    } = this.props;
+
+    const url = `/api/v1/show/${id}`;
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => this.setState({ tournament: response }))
+      .catch(() => this.props.history.push("/tournaments"));
+  }
+
+  addHtmlEntities(str) {
+    return String(str)
+      .replace(/&lt;/g, "<")
+      .replace(/&gt;/g, ">");
+  }
+
+  render() {
+    const { tournament: tournament } = this.state;
+    let teamList = "No teams available";
+
+    if (tournament.team.length > 0) {
+      teamList = tournament.team
+        .split(",")
+        .map((team, index) => (
+          <li key={index} className="list-group-item">
+            {team}
+          </li>
+        ));
+    }
+    const tournamentInstruction = this.addHtmlEntities(tournament.instruction);
+
+    return (
+      <div className="">
+        <div className="hero position-relative d-flex align-items-center justify-content-center">
+          <img
+            src={tournament.image}
+            alt={`${tournament.name} image`}
+            className="img-fluid position-absolute"
+          />
+          <div className="overlay bg-dark position-absolute" />
+          <h1 className="display-4 position-relative text-white">
+            {tournament.name}
+          </h1>
+        </div>
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-sm-12 col-lg-3">
+              <ul className="list-group">
+                <h5 className="mb-2">Teams</h5>
+                {teamList}
+              </ul>
+            </div>
+            <div className="col-sm-12 col-lg-7">
+              <h5 className="mb-2">Tournament Instructions</h5>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: `${tournamentInstruction}`
+                }}
+              />
+            </div>
+            <div className="col-sm-12 col-lg-2">
+              <button type="button" className="btn btn-danger">
+                Delete Tournament
+              </button>
+            </div>
+          </div>
+          <Link to="/tournaments" className="btn btn-link">
+            Back to tournaments
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Tournament;
--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Tournaments from "../components/Tournaments";
+import Tournament from "../components/Tournament";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tournaments" exact component={Tournaments} />
+      <Route path="/tournament/:id" exact component={Tournament} />
     </Switch>
   </Router>
 );
